f13edeadd77b4eb5216b2e7ceaecb49d879b9d5a
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2021-06-11 17:46:21 +0300

    fix: index file

---
+++ index.ts
@@ -0,0 +1,2 @@
+export { PubSubTransport, PubSubTransportConfig } from './src/pubsub-transport';
+export { PubSubTransportAsyncOptions, PubsubTransportModule } from './src/pubsub-transport.module';
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { PubSubTransport, PubSubTransportConfig } from './pubsub-transport';
-export { PubSubTransportAsyncOptions, PubsubTransportModule } from './pubsub-transport.module';
