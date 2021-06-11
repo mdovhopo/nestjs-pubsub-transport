9be9db08c85b95deef1a6442969cb57185bfb90a
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2021-06-11 17:13:52 +0300

    fix: nest DI

--- src/pubsub-transport.module.ts
+++ src/pubsub-transport.module.ts
@@ -75,6 +75,7 @@ export class PubsubTransportModule {
           useExisting,
           provide: PubSubTransportConfig,
         },
+        PubSubTransport,
       ],
       exports: [PubSubTransport],
     };
