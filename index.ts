4d98bdc0d60213dacd887e298f4e1a7acf5b2346
Vladyslav Bushynskyi <vladyslav.bushynskyi.ext@extendaretail.com>
2022-09-19 16:43:34 +0300

    fix: export PubSubContext interface

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { PubSubInterceptor } from './src/pubsub.interceptor';
-export { PubSubTransport, PubSubTransportConfig } from './src/pubsub-transport';
+export { PubSubTransport, PubSubTransportConfig, PubSubContext } from './src/pubsub-transport';
 export { PubSubTransportAsyncOptions, PubsubTransportModule } from './src/pubsub-transport.module';
